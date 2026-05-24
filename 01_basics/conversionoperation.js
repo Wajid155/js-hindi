478fa4abba7b0229b01d42a45eac982529b80e58
Wajid Hussain <wajid732084@gmail.com>
2026-05-24 10:30:44 +0000

    conersion of datatypes

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,29 @@
+let  score = "33abc"
+
+// console.log(typeof(score))
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber)
+
+
+// "33" => 33
+// "331bc" => NaN(not a number)
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/* 1 => true; 0 => false;
+"" => false
+"wajid" => true
+*/
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
