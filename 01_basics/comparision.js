b4abc58d9c1bb8cf6bc223c5710662599cbaa887
Wajid Hussain <wajid732084@gmail.com>
2026-05-24 19:22:58 +0000

    this is about

--- 01_basics/comparision.js
+++ 01_basics/comparision.js
@@ -8,5 +8,31 @@
 
 
 
+// *************************** Memory allocation *******************************
 
- 
+// Stack(Premitive data tyoe), Heap(Non premitive data type)
+
+  let myYoutubeName = "wajidhussaincom"
+
+  let anotherName = myYoutubeName
+  anotherName = "chaiaurcode"
+
+  console.log(myYoutubeName);
+  
+  console.log(anotherName);
+
+  let user1 = {
+    email: "wajid@example.com",
+    upi: "user@ybl"
+  }
+
+  let user2 = user1
+
+  user2.email = "user@example.com"
+
+  console.log(user1.email)
+ console.log(user2.email)
+
+
+  
+
