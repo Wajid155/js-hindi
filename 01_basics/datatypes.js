f1f3a3b5583d06717474cd04c8871dc4c7851a85
Wajid Hussain <wajid732084@gmail.com>
2026-05-24 09:46:44 +0000

    datatypes and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treate all JS code as newer version 
+
+console.log(3 + 3);
+console.log("Wajid");
+
+let num = 5
+let name = "wajid"
+let number = null
+
+console.log(typeof num , number, );
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "wajid@example.com"
+var accountPassword = "12345"
+accountCity = "Siwan"
+let accountState
+
+// accountId = 2
+accountEmail = "waj@example.com"
+accountPassword = "12121212"
+accountCity = "mumbai"
+
+/*
+prefer not to use var but mostly user let
+*/
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
