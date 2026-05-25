dca2b33e30e101b06410482bbf0e5eec67591992
Wajid Hussain <wajid732084@gmail.com>
2026-05-25 12:11:44 +0000

    array in js

---
+++ 02_basics/array.js
@@ -0,0 +1,43 @@
+const myArr = [0 , 1, 2, 3, 4, 5]
+const myHeroes = ["shaktiman", "iron man"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[0]);
+
+// Array methods 
+// myArr.push(7)
+// myArr.unshift(9)
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// slice and splice
+
+console.log("A" , myArr);
+
+const myn1 =  myArr.slice(1,3)
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("c", myArr);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
