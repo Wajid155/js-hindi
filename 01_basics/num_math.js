4143e25aa3e5c6eb217393fbeb422678f737daf1
Wajid Hussain <wajid732084@gmail.com>
2026-05-25 10:01:25 +0000

    math and num

---
+++ 01_basics/num_math.js
@@ -0,0 +1,48 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.89454
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundred = 100000
+// console.log(hundred.toLocaleString(`en-IN`));
+
+// **************************** Maths *****************************************
+// console.log(Math.PI);
+// console.log(Math.ceil(4.94));
+// console.log(Math.floor(4.94));
+
+// console.log(Math.min(3,5,1,6))
+// console.log(Math.max(3,8,4,7));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10 + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
