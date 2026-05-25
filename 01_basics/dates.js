e9e622951c6585aa62e1ef5197cb67a3edcd949c
Wajid Hussain <wajid732084@gmail.com>
2026-05-25 11:08:27 +0000

    dates in js

---
+++ 01_basics/dates.js
@@ -0,0 +1,32 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+
+// let myCreatedDate = new Date(2026,0,1)
+// let newdate = new Date(2026,0,25,5,3)
+// console.log(myCreatedDate.toLocaleString());
+// console.log(newdate.toLocaleString());
+
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMilliseconds());
+
+
+
+
+
+
+
+
