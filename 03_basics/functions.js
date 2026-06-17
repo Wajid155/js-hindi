628e329a36e1311ef22e61038622679a26d792de
Wajid Hussain <wajid732084@gmail.com>
2026-06-17 14:41:36 +0000

    functions in js

---
+++ 03_basics/functions.js
@@ -0,0 +1,36 @@
+// function add2num(num1, num2){
+
+// return num1 + num2
+// }
+//  const result = add2num(5, 3) 
+// //  console.log(result);
+
+//  function loginUserMessage(username) {
+//     if(!username) {
+//         console.log("pls enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+//  }
+
+// console.log( loginUserMessage(""))
+
+function calculateCartPrice(val1, val2 ,... num1) {
+    return num1
+
+}
+
+console.log(calculateCartPrice(200,100,20,500));
+
+const user = {
+    username: "wajid",
+    age: 20
+
+}
+
+function handleObject(anyuser) {
+    console.log(`username is ${anyuser.username} and the age is ${anyuser.age}`);
+    
+}
+
+handleObject(user)
