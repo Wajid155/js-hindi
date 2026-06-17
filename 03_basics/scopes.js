e72ffccfd171438adf97a618e82c6a9d8a512025
Wajid Hussain <wajid732084@gmail.com>
2026-06-17 15:35:28 +0000

    scopes in js

---
+++ 03_basics/scopes.js
@@ -0,0 +1,37 @@
+let a = 300
+
+if (true) {
+    let a = 30
+    let b = 20
+
+    // console.log("Inner: ", a);
+    
+}
+
+// console.log(a);
+
+
+function one() {
+    const username = "wajid"
+
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    two()
+}
+
+if(true) {
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website = "youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website); 
+    
+}
+
+// console.log(username);
+
